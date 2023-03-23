2adead288a5cbf9d87170ff324d57b32dbc64248
elkoutaibifatimazahra <elkoutaibifatimazahra@gmail.com>
2023-03-23 06:38:30 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - print 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers - print 10 times the numbers, from 0 to 14, followed by a new line
  */
 
 void more_numbers(void)
